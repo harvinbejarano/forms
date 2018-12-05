5d053f07b2f11562f4f365b31b910102ede0e6c6
Harvin Bejarano <harvinbejarano@gmail.com>
2018-12-05 18:26:09 -0500

    reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: [ './app.component.scss' ],
 })
 export class AppComponent {
-  title = 'forms';
+	form: FormGroup;
+
+	constructor(private fb: FormBuilder) {
+		this.form = this.fb.group({
+			username: [ '', [ Validators.required ] ],
+			password: [ '', [ Validators.required ] ],
+		});
+
+		this.form.patchValue({
+			username: 'Harvin',
+			password: '123',
+		});
+	}
+
+	onSubmit(event) {
+		console.log(event);
+	}
 }
